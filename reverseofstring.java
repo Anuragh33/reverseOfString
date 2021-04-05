f8d3d0c7f9e44173d1e31f3641d2414aaba15220
Ragidimilli Anuragh <Princeanuragh1@gmail.com>
2021-04-05 16:41:11 +0530

    Update reverseofstring.java

--- reverseofstring.java
+++ reverseofstring.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 class Main {
   public static void main(String[] args) {
-  String str;
+  String str, sa = "";
   char[] ch;
-  String sa = "";
+  
   Scanner s = new Scanner(System.in);
   System.out.println("Enter the String you would like to reverse: ");
   str = s.nextLine();
   ch = str.toCharArray();
   System.out.println(" The Reversed String is: ");
   for (int i = ch.length-1 ; i >= 0; i--){
-    sa = sa + ch[i];
+    sa += ch[i];
     }
    System.out.println(sa);
   }
